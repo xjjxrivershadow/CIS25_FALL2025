ac40c952ae65be5fefd212f0f6777fa7db315b4d
Aaron <xjjxrivershadow@gmail.com>
2025-09-03 17:39:42 -0700

    Add files via upload

---
+++ HelloInventorySystem.cpp
@@ -0,0 +1,14 @@
+// HelloInventorySystem!.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+	cout << "Hello Inventory System!\n";
+	cout << "\tData Types:\n";
+	cout << "Size of int: " << sizeof(int) << " bytes\n";
+	cout << "Size of float: " << sizeof(float) << " bytes\n";
+	cout << "Size of char: " << sizeof(char) << " bytes\n";
+	return 0;
+}
